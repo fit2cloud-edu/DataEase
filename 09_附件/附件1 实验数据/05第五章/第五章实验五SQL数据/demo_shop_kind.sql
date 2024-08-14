528d36e665fd20adfac53547efc39b4489a5730a
谢贝 <xiebei0710@163.com>
2024-08-14 17:39:55 +0800

    初始化 DataEase 高校教材仓库

---
+++ 09_附件/附件1 实验数据/05第五章/第五章实验五SQL数据/demo_shop_kind.sql
@@ -0,0 +1,3 @@
+INSERT INTO class_demo.demo_shop_kind (id,name,commission_percent) VALUES
+	 ('f607ffcc-b555-11ec-a23d-3102764b1481','加盟店',0.8),
+	 ('f609cd2a-b555-11ec-a23d-3102764b1481','直营店',1.0);
